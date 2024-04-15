f5cca233fcb4a749db0bcd41a0bdb9fc6bc6269e
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-15 10:47:10 +0300

    products store: state, actions, reducer, effects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/products' },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./pages/products/products.component').then(
+        (c) => c.ProductsComponent
+      ),
+  },
+];
---
+++ src/app/core/constants/api.constant.ts
@@ -0,0 +1,1 @@
+export const BASE_URL_API = 'https://api.escuelajs.co/api/v1';
