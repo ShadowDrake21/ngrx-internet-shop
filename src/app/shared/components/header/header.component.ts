6a0704207bb35790fb49907da9aafd95470022ff
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-16 09:14:53 +0300

    header and footer

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faCartShopping,
+  faSignInAlt,
+  faSignOut,
+  faSignOutAlt,
+  faUserAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  cart = faCartShopping;
+  signIn = faSignInAlt;
+  signOut = faSignOutAlt;
+  profile = faUserAlt;
 }
