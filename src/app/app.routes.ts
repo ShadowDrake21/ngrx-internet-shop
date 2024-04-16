dd9a7acc907a4a69fb8903c26dcdbe9b429cd5b2
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-16 09:14:53 +0300

    header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/products' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
   {
     path: 'products',
     loadComponent: () =>
@@ -14,6 +19,13 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/product/product.component').then(
         (c) => c.ProductComponent
+      ),
+  },
+  {
+    path: 'categories',
+    loadComponent: () =>
+      import('./pages/categories/categories.component').then(
+        (c) => c.CategoriesComponent
       ),
   },
 ];
