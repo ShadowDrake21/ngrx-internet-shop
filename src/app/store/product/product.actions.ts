b3bf0ce704e5598682e31b8280f48cb5bd9ae642
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-17 21:29:55 +0300

    product filter: part 2, service + additions to the product store

--- src/app/store/product/product.actions.ts
+++ src/app/store/product/product.actions.ts
@@ -1,12 +1,25 @@
 import { createAction, props } from '@ngrx/store';
 import { IProduct } from '../../shared/models/product.model';
+import { IFilterFormObj } from '../../shared/models/forms.model';
 
-export const loadProduct = createAction('[Products Component] loadProducts');
+export const loadProduct = createAction('[Products Component] LoadProducts');
 export const loadProductSuccess = createAction(
-  '[Products Component] loadProductsSuccess',
+  '[Products Component] LoadProductsSuccess',
   props<{ products: IProduct[] }>()
 );
 export const loadProductFailure = createAction(
-  '[Products Component] loadProductsFailure',
+  '[Products Component] LoadProductsFailure',
+  props<{ errorMessage: string }>()
+);
+export const filterProducts = createAction(
+  '[Products Component] FilterProducts',
+  props<{ filterData: IFilterFormObj }>()
+);
+export const filterProductsSuccess = createAction(
+  '[Products Component] FilterProductsSuccess',
+  props<{ products: IProduct[] }>()
+);
+export const filterProductsFailure = createAction(
+  '[Products Component] FilterProductsFailure',
   props<{ errorMessage: string }>()
 );
