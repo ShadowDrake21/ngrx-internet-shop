af2c6ac15ed62e6febb552cfbea6b544b44406f0
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-24 14:14:11 +0300

    sign-in page: facebook sign in, part 1

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -1,12 +1,15 @@
 import { inject, Injectable } from '@angular/core';
 import {
   Auth,
+  FacebookAuthProvider,
   sendPasswordResetEmail,
   signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
   UserCredential,
 } from '@angular/fire/auth';
-import { from, Observable } from 'rxjs';
+import { FirebaseError } from 'firebase/app';
+import { catchError, from, map, Observable, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -18,6 +21,30 @@ export class AuthService {
 
   sendPasswordReset(email: string): Observable<void> {
     return from(sendPasswordResetEmail(this.auth, email));
+  }
+
+  signInViaFacebook(): Observable<{
+    userCredential: UserCredential;
+    accessToken: string | undefined;
+  }> {
+    return from(signInWithPopup(this.auth, new FacebookAuthProvider())).pipe(
+      map((result: UserCredential) => {
+        const user = result.user;
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential?.accessToken;
+
+        return { userCredential: result, accessToken };
+      }),
+      catchError((error: FirebaseError) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        console.log('Firebase Authentication Credential:', credential);
+
+        return throwError(error);
+      })
+    );
   }
 
   signOut(): Observable<void> {
