7fc98a2eecc69124fe73e98327e11919a56bc006
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-28 11:36:43 +0300

    home-page: product of the day, can be interesing product

--- src/app/pages/home/components/product-slider/content/product-slider.content.ts
+++ src/app/pages/home/components/product-slider/content/product-slider.content.ts
@@ -1,8 +1,6 @@
 export interface IProductSliderImage {
   path: string;
   author: string;
-  width: number;
-  height: number;
 }
 
 export const productSliderImages: IProductSliderImage[] = [
@@ -10,70 +8,50 @@ export const productSliderImages: IProductSliderImage[] = [
     path: '/assets/images/slider-images/slider-img-1.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@mikepetrucci?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Mike Petrucci</a> on <a href="https://unsplash.com/photos/gray-and-blue-open-signage-c9FQyqIECds?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 2304,
-    height: 1536,
   },
   {
     path: '/assets/images/slider-images/slider-img-2.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@mercantile?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Clark Street Mercantile</a> on <a href="https://unsplash.com/photos/clothes-store-interior-P3pI6xzovu0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 7360,
-    height: 4912,
   },
   {
     path: '/assets/images/slider-images/slider-img-3.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@pmvch?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">pmv chamara</a> on <a href="https://unsplash.com/photos/two-black-and-gold-tube-bottles-3aFn1zWRuoc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 5460,
-    height: 3642,
   },
   {
     path: '/assets/images/slider-images/slider-img-4.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@nordwood?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">NordWood Themes</a> on <a href="https://unsplash.com/photos/pair-of-brown-leather-boots-beside-bet-Nv4QHkTVEaI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 4773,
-    height: 3182,
   },
   {
     path: '/assets/images/slider-images/slider-img-5.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@rocinante_11?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Mick Haupt</a> on <a href="https://unsplash.com/photos/white-and-blue-plastic-packs-WXnBzI7AypY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 5250,
-    height: 3937,
   },
   {
     path: '/assets/images/slider-images/slider-img-6.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@dominostudio?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Domino Studio</a> on <a href="https://unsplash.com/photos/unpaired-red-nike-sneaker-164_6wVEHfI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 5472,
-    height: 3648,
   },
   {
     path: '/assets/images/slider-images/slider-img-7.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@leecampbell?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Lee  Campbell</a> on <a href="https://unsplash.com/photos/gray-and-brown-corded-headphones-GI6L2pkiZgQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 4096,
-    height: 2732,
   },
   {
     path: '/assets/images/slider-images/slider-img-8.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@piak?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Josip Ivankovic</a> on <a href="https://unsplash.com/photos/national-korea-library-jpeTs-VfP68?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 5472,
-    height: 3648,
   },
   {
     path: '/assets/images/slider-images/slider-img-9.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@maksimcul8r?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Maksim Larin</a> on <a href="https://unsplash.com/photos/champion-concrete-white-painted-store-tecILYzVAzg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 4896,
-    height: 3497,
   },
   {
     path: '/assets/images/slider-images/slider-img-10.jpg',
     author:
       'Photo by <a href="https://unsplash.com/@freestocks?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">freestocks</a> on <a href="https://unsplash.com/photos/photo-of-woman-holding-white-and-black-paper-bags-_3Q3tsJ01nc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Unsplash</a>',
-    width: 5185,
-    height: 3457,
   },
 ];
