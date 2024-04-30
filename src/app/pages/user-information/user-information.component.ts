4e2eec90c3b901ef83319538d6dd04c383ea84f1
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-30 19:06:27 +0300

    user-information page: active route, height complications

--- src/app/pages/user-information/user-information.component.ts
+++ src/app/pages/user-information/user-information.component.ts
@@ -1,6 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
+import { RoutingService } from '@app/core/services/routing.service';
+import { FooterComponent } from '@app/shared/components/footer/footer.component';
+import { HeaderComponent } from '@app/shared/components/header/header.component';
 import { userInformationSidebar } from '@app/shared/utils/icons.utils';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TabsModule } from 'ngx-bootstrap/tabs';
@@ -9,15 +17,26 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
   selector: 'app-user-information',
   standalone: true,
   imports: [
-    RouterOutlet,
     CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
     FontAwesomeModule,
     TabsModule,
     RouterLink,
+    RouterLinkActive,
   ],
   templateUrl: './user-information.component.html',
   styleUrl: './user-information.component.scss',
 })
-export class UserInformationComponent {
+export class UserInformationComponent implements OnInit {
+  private routingService = inject(RoutingService);
+
+  previousRoute!: string;
+
+  ngOnInit(): void {
+    this.previousRoute = this.routingService.getPreviousUrl() ?? '/';
+  }
+
   sidebarIcons = userInformationSidebar;
 }
