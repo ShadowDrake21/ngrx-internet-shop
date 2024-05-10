dd9a9014fc4c3997d7948a2701d92f3bc86fe549
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-05-10 14:38:11 +0300

    purchases:  transactions, part 4 (product loading to db)

--- server/controllers/purchaseControllers.js
+++ server/controllers/purchaseControllers.js
@@ -1,11 +1,10 @@
-const { default: firebase } = require("../firebase");
-const { default: Purchase } = require("../models/purchaseModel");
-const { getDatabase, set, ref, get, push } = require("firebase/database");
+import firebase from "../firebase.js";
+import { getDatabase, set, ref, get, push } from "firebase/database";
 
 const db = getDatabase(firebase);
 
-const createPurchase = async (products, email, session_id) => {
-  await set(ref(db, `users/${email}/purchases/${session_id}`), data);
+const createPurchase = async (products, id, session_id) => {
+  await set(ref(db, `customers/${id}/purchases/${session_id}`), products);
 };
 
 export default createPurchase;
