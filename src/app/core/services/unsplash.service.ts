9c86fe528de029f881910f957f9158b8f7382aa2
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-05-14 10:22:10 +0300

    delivery-details: functionality, part 2 (unsplash implementation)

---
+++ src/app/core/services/unsplash.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BASE_UNSPLASH_URL } from '../constants/unsplash.constants';
+import { environment } from 'environments/environment.development';
+import { Observable } from 'rxjs';
+import { IUnsplashImageResponse } from '@app/shared/models/unsplash.model';
+
+@Injectable({ providedIn: 'root' })
+export class UnsplashService {
+  private http = inject(HttpClient);
+
+  getPhotoArray(
+    location: string,
+    orientation: 'landscape' | 'portrait' | 'squarish' = 'landscape'
+  ): Observable<IUnsplashImageResponse> {
+    return this.http.get<IUnsplashImageResponse>(
+      BASE_UNSPLASH_URL + 'search/photos/',
+      {
+        params: new HttpParams()
+          .set('query', location)
+          .set('orientation', orientation)
+          .set('client_id', environment.unsplash.accessKey),
+      }
+    );
+  }
+}
