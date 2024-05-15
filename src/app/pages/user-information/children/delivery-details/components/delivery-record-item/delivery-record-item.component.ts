e8c4d1b955712af50b35df0fa2f57fccb0522089
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-05-15 14:44:39 +0300

    delivery-details: functionality, part 7, done

---
+++ src/app/pages/user-information/children/delivery-details/components/delivery-record-item/delivery-record-item.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { IShipping } from '@app/shared/models/purchase.model';
+import { deliveryDetailsIcons } from '@app/shared/utils/icons.utils';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-delivery-record-item',
+  standalone: true,
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './delivery-record-item.component.html',
+  styleUrl: './delivery-record-item.component.scss',
+})
+export class DeliveryRecordListComponent {
+  icons = deliveryDetailsIcons;
+
+  @Input({ required: true }) record!: IShipping;
+
+  @Output() recordRemove: EventEmitter<string> = new EventEmitter<string>();
+  @Output() recordEdit: EventEmitter<string> = new EventEmitter<string>();
+}
