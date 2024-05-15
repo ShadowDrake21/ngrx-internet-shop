589670e13c024b2131d97832a38da5aa52aff50a
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-05-15 14:44:39 +0300

    delivery-details: functionality, part 7, done

--- src/app/pages/user-information/children/delivery-details/components/delivery-record-form/delivery-record-form.component.ts
+++ src/app/pages/user-information/children/delivery-details/components/delivery-record-form/delivery-record-form.component.ts
@@ -1,12 +1,198 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { DatabaseService } from '@app/core/services/database.service';
+import { phonePattern } from '../../../purchases/components/customer-information/constants/pattern.constants';
+import { map, Observable, of, Subscription, switchMap } from 'rxjs';
+import { IShipping } from '@app/shared/models/purchase.model';
+import { IReducedUnsplashImage } from '@app/shared/models/unsplash.model';
+import { shuffleArray } from '@app/shared/utils/arrayManipulations.utils';
+import { UnsplashService } from '@app/core/services/unsplash.service';
 
 @Component({
   selector: 'app-delivery-record-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './delivery-record-form.component.html',
-  styleUrl: './delivery-record-form.component.scss'
+  styleUrl: './delivery-record-form.component.scss',
 })
-export class DeliveryRecordFormComponent {
+export class DeliveryRecordFormComponent
+  implements OnInit, OnChanges, OnDestroy
+{
+  @Input({ required: true }) customerId: string = '';
+  @Input({ required: true }) formEnableValue: 'enable' | 'disable' = 'enable';
+  @Input() recordForEditing!: IShipping | null;
 
+  private databaseService = inject(DatabaseService);
+  private unsplashService = inject(UnsplashService);
+
+  @Output() sendNewDeliveryRecord: EventEmitter<{
+    record: IShipping;
+    mode: 'edit' | 'add';
+  }> = new EventEmitter<{ record: IShipping; mode: 'edit' | 'add' }>();
+
+  isEditMode: boolean = false;
+
+  shippingForm = new FormGroup({
+    id: new FormControl('', [Validators.required]),
+    name: new FormControl('', [
+      Validators.minLength(3),
+      Validators.maxLength(40),
+      Validators.required,
+    ]),
+    phone: new FormControl('', [
+      Validators.pattern(phonePattern),
+      Validators.required,
+    ]),
+    address: new FormGroup({
+      country: new FormControl('0', [Validators.required]),
+      city: new FormControl('', Validators.required),
+      line1: new FormControl('', Validators.required),
+      line2: new FormControl('', Validators.required),
+      postalCode: new FormControl('', Validators.required),
+    }),
+  });
+
+  private subscriptions: Subscription[] = [];
+
+  ngOnInit(): void {
+    this.shippingForm.controls.id.patchValue(
+      `delivery-record_${new Date().getTime()}`
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['recordForEditing']) {
+      if (this.recordForEditing) {
+        this.isEditMode = true;
+        this.patchEditRecordToForm(this.recordForEditing);
+      }
+    }
+
+    if (changes['formEnableValue']) {
+      if (this.formEnableValue === 'enable') {
+        this.shippingForm.enable();
+      } else {
+        this.shippingForm.disable();
+      }
+    }
+  }
+
+  onSubmit() {
+    const submitSubscription = this.getDeliveryRecordBackground(
+      this.shippingForm.value.address?.country!
+    )
+      .pipe(
+        switchMap((background) => this.formDeliveryRecord(background)),
+        switchMap((newDeliveryRecord) => of({ newDeliveryRecord }))
+      )
+      .subscribe(({ newDeliveryRecord }) => {
+        this.databaseService.setDeliveryRecord(
+          newDeliveryRecord,
+          this.customerId,
+          newDeliveryRecord.id!
+        );
+
+        if (this.isEditMode) {
+          this.sendNewDeliveryRecord.emit({
+            record: newDeliveryRecord,
+            mode: 'edit',
+          });
+        } else {
+          this.sendNewDeliveryRecord.emit({
+            record: newDeliveryRecord,
+            mode: 'add',
+          });
+        }
+        this.onFormReset();
+      });
+
+    this.subscriptions.push(submitSubscription);
+  }
+
+  getDeliveryRecordBackground(
+    countryCode: string
+  ): Observable<IReducedUnsplashImage> {
+    const country = countryCode === 'PL' ? 'Poland' : 'Ukraine';
+
+    return this.unsplashService.getPhotoArray(country).pipe(
+      map(({ results }) => shuffleArray(results)[0]),
+      map((item) => this.formBackgroundObject(item))
+    );
+  }
+
+  formBackgroundObject(item: any): IReducedUnsplashImage {
+    return {
+      title: item.slug,
+      url: item.urls.full,
+      user: {
+        name: item.user.username,
+        link: item.user.links.html,
+      },
+    };
+  }
+
+  formDeliveryRecord(
+    backgroundObj: IReducedUnsplashImage
+  ): Observable<IShipping> {
+    return of({
+      background: backgroundObj,
+      id: this.shippingForm.value.id!,
+      name: this.shippingForm.value.name!,
+      phone: this.shippingForm.value.phone!,
+      address: {
+        country: this.shippingForm.value.address?.country!,
+        city: this.shippingForm.value.address?.city!,
+        line1: this.shippingForm.value.address?.line1!,
+        line2: this.shippingForm.value.address?.line2!,
+        postal_code: this.shippingForm.value.address?.postalCode!,
+      },
+    });
+  }
+
+  onFormReset() {
+    if (this.isEditMode) {
+      this.isEditMode = false;
+    }
+    this.shippingForm.reset();
+    this.shippingForm.controls.address.controls.country.patchValue('0');
+    this.shippingForm.controls.id.patchValue(
+      `delivery-record_${new Date().getTime()}`
+    );
+  }
+
+  patchEditRecordToForm(record: IShipping) {
+    const { id, name, phone, address } = record;
+    this.shippingForm.patchValue({
+      id,
+      name,
+      phone,
+      address: {
+        country: address.country,
+        city: address.city,
+        line1: address.line1,
+        line2: address.line2,
+        postalCode: address.postal_code,
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
--- src/app/pages/user-information/children/delivery-details/delivery-details.component.ts
+++ src/app/pages/user-information/children/delivery-details/delivery-details.component.ts
@@ -1,32 +1,13 @@
 import { CommonModule } from '@angular/common';
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  inject,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { BasicCardComponent } from '../../components/basic-card/basic-card.component';
 import { userInformationContent } from '../../content/user-information.content';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { phonePattern } from '../purchases/components/customer-information/constants/pattern.constants';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DatabaseService } from '@app/core/services/database.service';
 import {
-  BehaviorSubject,
-  filter,
-  findIndex,
   map,
   Observable,
   of,
-  startWith,
   Subscription,
   switchMap,
   tap,
@@ -37,17 +18,10 @@ import { Store } from '@ngrx/store';
 
 import * as PurchaseSelectors from '@store/purchase/purchase.selectors';
 import { PurchaseState } from '@app/store/purchase/purchase.reducer';
-import { UnsplashService } from '@app/core/services/unsplash.service';
-import { IReducedUnsplashImage } from '@app/shared/models/unsplash.model';
-import { shuffleArray } from '@app/shared/utils/arrayManipulations.utils';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { deliveryDetailsIcons } from '@app/shared/utils/icons.utils';
-import {
-  PageChangedEvent,
-  PaginationComponent,
-  PaginationModule,
-} from 'ngx-bootstrap/pagination';
+import { DeliveryRecordFormComponent } from './components/delivery-record-form/delivery-record-form.component';
+import { DeliveryRecordListComponent } from './components/delivery-record-item/delivery-record-item.component';
+
 @Component({
   selector: 'app-delivery-details',
   standalone: true,
@@ -56,53 +30,28 @@ import {
     BasicCardComponent,
     ReactiveFormsModule,
     FontAwesomeModule,
+    DeliveryRecordFormComponent,
+    DeliveryRecordListComponent,
   ],
   templateUrl: './delivery-details.component.html',
   styleUrl: './delivery-details.component.scss',
 })
 export class DeliveryDetailsComponent implements OnInit, OnDestroy {
   userInformationItem = userInformationContent[3];
-  icons = deliveryDetailsIcons;
 
   private store = inject(Store<PurchaseState>);
   private databaseService = inject(DatabaseService);
-  private unsplashService = inject(UnsplashService);
 
-  isEditMode: boolean = false;
-
-  shippingForm = new FormGroup({
-    id: new FormControl('', [Validators.required]),
-    name: new FormControl('', [
-      Validators.minLength(3),
-      Validators.maxLength(40),
-      Validators.required,
-    ]),
-    phone: new FormControl('', [
-      Validators.pattern(phonePattern),
-      Validators.required,
-    ]),
-    address: new FormGroup({
-      country: new FormControl('0', [Validators.required]),
-      city: new FormControl('', Validators.required),
-      line1: new FormControl('', Validators.required),
-      line2: new FormControl('', Validators.required),
-      postalCode: new FormControl('', Validators.required),
-    }),
-  });
-
-  private customerId: string = '';
-
+  customerId: string = '';
   deliveryRecords$!: Observable<IShipping[]>;
-  isFormEnable: boolean = true;
   sizeRestriction: number = 6;
+
+  formEnableValue: 'enable' | 'disable' = 'enable';
+  recordForEditing!: IShipping;
 
   private subscriptions: Subscription[] = [];
 
   ngOnInit(): void {
-    this.shippingForm.controls.id.patchValue(
-      `delivery-record_${new Date().getTime()}`
-    );
-
     const customerSubscription = this.store
       .select(PurchaseSelectors.selectCustomer)
       .subscribe((customer) => {
@@ -111,115 +60,19 @@ export class DeliveryDetailsComponent implements OnInit, OnDestroy {
           this.deliveryRecords$ = this.databaseService.getAllDeliveryRecords(
             this.customerId
           );
-          this.deliveryRecords$.subscribe((records) => {
-            if (records.length >= 6) {
-              this.shippingForm.disable();
+          const deliveryRecordsSubscription = this.deliveryRecords$.subscribe(
+            (records) => {
+              if (records.length >= 6) {
+                this.formEnableValue = 'disable';
+              }
             }
-          });
+          );
+
+          this.subscriptions.push(deliveryRecordsSubscription);
         }
       });
 
     this.subscriptions.push(customerSubscription);
-  }
-
-  onSubmit() {
-    const submitSubscription = this.getDeliveryRecordBackground(
-      this.shippingForm.value.address?.country!
-    )
-      .pipe(
-        switchMap((background) => this.formDeliveryRecord(background)),
-        switchMap((newDeliveryRecord) =>
-          this.deliveryRecords$.pipe(
-            map(() => ({
-              newDeliveryRecord,
-            }))
-          )
-        )
-      )
-      .subscribe(({ newDeliveryRecord }) => {
-        this.databaseService.setDeliveryRecord(
-          newDeliveryRecord,
-          this.customerId,
-          newDeliveryRecord.id!
-        );
-
-        if (this.isEditMode) {
-          this.deliveryRecords$ = this.deliveryRecords$.pipe(
-            switchMap((records) => {
-              const recordIndex = records.findIndex(
-                (record) => record.id === newDeliveryRecord.id
-              );
-              if (recordIndex !== -1) {
-                const updatedRecords = [...records];
-                updatedRecords[recordIndex] = newDeliveryRecord;
-                return of(updatedRecords);
-              } else {
-                return throwError(
-                  () =>
-                    new Error(
-                      `Record with id ${newDeliveryRecord.id} not found.`
-                    )
-                );
-              }
-            })
-          );
-        } else {
-          this.deliveryRecords$ = this.deliveryRecords$.pipe(
-            map((existingRecords) => {
-              return [...existingRecords, newDeliveryRecord];
-            }),
-            tap((newRecordsArray) => {
-              if (newRecordsArray.length === 6) {
-                this.shippingForm.disable();
-              }
-            })
-          );
-        }
-
-        this.onFormReset();
-      });
-
-    this.subscriptions.push(submitSubscription);
-  }
-
-  getDeliveryRecordBackground(
-    countryCode: string
-  ): Observable<IReducedUnsplashImage> {
-    const country = countryCode === 'PL' ? 'Poland' : 'Ukraine';
-
-    return this.unsplashService.getPhotoArray(country).pipe(
-      map(({ results }) => shuffleArray(results)[0]),
-      map((item) => this.formBackgroundObject(item))
-    );
-  }
-
-  formBackgroundObject(item: any): IReducedUnsplashImage {
-    return {
-      title: item.slug,
-      url: item.urls.full,
-      user: {
-        name: item.user.username,
-        link: item.user.links.html,
-      },
-    };
-  }
-
-  formDeliveryRecord(
-    backgroundObj: IReducedUnsplashImage
-  ): Observable<IShipping> {
-    return of({
-      background: backgroundObj,
-      id: this.shippingForm.value.id!,
-      name: this.shippingForm.value.name!,
-      phone: this.shippingForm.value.phone!,
-      address: {
-        country: this.shippingForm.value.address?.country!,
-        city: this.shippingForm.value.address?.city!,
-        line1: this.shippingForm.value.address?.line1!,
-        line2: this.shippingForm.value.address?.line2!,
-        postal_code: this.shippingForm.value.address?.postalCode!,
-      },
-    });
   }
 
   removeDeliveryRecord(id: string) {
@@ -229,7 +82,7 @@ export class DeliveryDetailsComponent implements OnInit, OnDestroy {
         this.deliveryRecords$ = this.deliveryRecords$.pipe(
           map((records) => {
             if (records.length === 6) {
-              this.shippingForm.enable();
+              this.formEnableValue = 'enable';
             }
             return records.filter((record) => record.id !== id);
           })
@@ -239,44 +92,63 @@ export class DeliveryDetailsComponent implements OnInit, OnDestroy {
     this.subscriptions.push(removeSubscription);
   }
 
-  editDeliveryRecord(id: string) {
-    const editSubscription = this.deliveryRecords$
-      .pipe(map((records) => records.find((record) => record.id === id)))
-      .subscribe((record) => {
-        if (record) {
-          this.isEditMode = true;
-          this.patchEditRecordToForm(record);
+  addNewRecord(newRecord: IShipping) {
+    this.deliveryRecords$ = this.deliveryRecords$.pipe(
+      map((existingRecords) => {
+        return [...existingRecords, newRecord];
+      }),
+      tap((newRecordsArray) => {
+        if (newRecordsArray.length === 6) {
+          this.formEnableValue = 'disable';
         }
-      });
-
-    this.subscriptions.push(editSubscription);
-  }
-
-  patchEditRecordToForm(record: IShipping) {
-    const { id, name, phone, address } = record;
-    this.shippingForm.patchValue({
-      id,
-      name,
-      phone,
-      address: {
-        country: address.country,
-        city: address.city,
-        line1: address.line1,
-        line2: address.line2,
-        postalCode: address.postal_code,
-      },
-    });
-  }
-
-  onFormReset() {
-    if (this.isEditMode) {
-      this.isEditMode = false;
-    }
-    this.shippingForm.reset();
-    this.shippingForm.controls.address.controls.country.patchValue('0');
-    this.shippingForm.controls.id.patchValue(
-      `delivery-record_${new Date().getTime()}`
+      })
     );
+  }
+
+  editExistingRecord(editRecord: IShipping) {
+    this.deliveryRecords$ = this.deliveryRecords$.pipe(
+      switchMap((records) => {
+        const recordIndex = records.findIndex(
+          (record) => record.id === editRecord.id
+        );
+        if (recordIndex !== -1) {
+          const updatedRecords = [...records];
+          updatedRecords[recordIndex] = editRecord;
+          return of(updatedRecords);
+        } else {
+          return throwError(
+            () => new Error(`Record with id ${editRecord.id} not found.`)
+          );
+        }
+      })
+    );
+  }
+
+  handleNewDeliveryRecord(object: { record: IShipping; mode: 'add' | 'edit' }) {
+    const { record, mode } = object;
+
+    if (mode === 'add') {
+      this.addNewRecord(record);
+    } else {
+      this.editExistingRecord(record);
+    }
+  }
+
+  handleEditRecordRequest(recordId: string) {
+    const handleEditRecordRequestSubscription: Subscription =
+      this.deliveryRecords$
+        .pipe(
+          map((records) => records.find((record) => record.id === recordId))
+        )
+        .subscribe((record) => {
+          this.recordForEditing = record!;
+        });
+
+    this.subscriptions.push(handleEditRecordRequestSubscription);
+  }
+
+  handleRemoveRecordRequest(recordId: string) {
+    this.removeDeliveryRecord(recordId);
   }
 
   ngOnDestroy(): void {
